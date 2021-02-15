9ef8d7ffc68d3482180bd5f7b661af50a4f89f5d
k00lawn <k00lawn@gmail.com>
2021-02-15 21:26:10 +0530

    first first steps

--- src/components/Ground.js
+++ src/components/Ground.js
@@ -1,14 +1,17 @@
+ 
 import React from 'react';
 import { usePlane } from 'use-cannon';
 import { TextureLoader, RepeatWrapping } from 'three';
 import grass from '../images/grass.jpg';
-
 import { useStore } from '../hooks/useStore';
 
-export default function Ground(props) {
+export const Ground = (props) => {
   const [ref] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0], ...props }));
-  const [addCube, type] = useStore((state) => [state.addCube, state.type]);
   const texture = new TextureLoader().load(grass);
+  const [addCube, activeTexture] = useStore((state) => [
+    state.addCube,
+    state.texture,
+  ]);
   texture.wrapS = RepeatWrapping;
   texture.wrapT = RepeatWrapping;
   texture.repeat.set(100, 100);
@@ -18,12 +21,14 @@ export default function Ground(props) {
       receiveShadow
       onClick={(e) => {
         e.stopPropagation();
-        const { x, y, z } = e.point;
-        addCube(Math.ceil(x), Math.ceil(y), Math.ceil(z), type);
+        const [x, y, z] = Object.values(e.point).map((coord) =>
+          Math.ceil(coord),
+        );
+        addCube(x, y, z, activeTexture);
       }}
     >
       <planeBufferGeometry attach="geometry" args={[100, 100]} />
       <meshStandardMaterial map={texture} attach="material" />
     </mesh>
   );
-}
+};
