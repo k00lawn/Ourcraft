2577341606663c8cabd5707c137a46fa1a38b4f1
k00lawn <k00lawn@gmail.com>
2021-02-25 23:53:16 +0530

    simplified server code

--- server/index.js
+++ server/index.js
@@ -1,129 +1,153 @@
-const http = require("http");
+//HTML elements
+let clientId = null;
+let gameId = null;
+let playerColor = null;
 
-const websocketServer = require("websocket").server
-const httpServer = http.createServer();
-httpServer.listen(9090, () => console.log("Listening.. on 9090"))
-//hashmap clients
-const clients = {};
-const games = {};
+let ws = new WebSocket("ws://localhost:9090")
+const btnCreate = document.getElementById("btnCreate");
+const btnJoin = document.getElementById("btnJoin");
+const txtGameId = document.getElementById("txtGameId");
+const divPlayers = document.getElementById("divPlayers");
+const divBoard = document.getElementById("divBoard");
 
-const wsServer = new websocketServer({
-    "httpServer": httpServer
-})
-wsServer.on("request", request => {
-    //connect
-    const connection = request.accept(null, request.origin);
-    connection.on("open", () => console.log("opened!"))
-    connection.on("close", () => console.log("closed!"))
-    connection.on("message", message => {
-        const result = JSON.parse(message.utf8Data)
-        //I have received a message from the client
-        //a user want to create a new game
-        if (result.method === "create") {
-            const clientId = result.clientId;
-            const gameId = guid();
-            games[gameId] = {
-                "id": gameId,
-                // "balls": 20,
-                "clients": []
-            }
+let state = [];
 
-            const payLoad = {
-                "method": "create",
-                "game" : games[gameId]
-            }
-
-            const con = clients[clientId].connection;
-            con.send(JSON.stringify(payLoad));
-        }
-
-        //a client want to join
-        if (result.method === "join") {
-
-            const clientId = result.clientId;
-            const gameId = result.gameId;
-            const game = games[gameId];            
-            const color =  {"0": "Red", "1": "Green", "2": "Blue"}[game.clients.length]
-            game.clients.push({
-                "clientId": clientId,
-                // "color": color
-            })
-            //start the game
-            if (game.clients.length > 0) updateGameState();
-
-            const payLoad = {
-                "method": "join",
-                "game": game
-            }
-            //loop through all clients and tell them that people has joined
-            game.clients.forEach(c => {
-                clients[c.clientId].connection.send(JSON.stringify(payLoad))
-            })
-        }
-        //a user plays
-        if (result.method === "play") {
-            const gameId = result.gameId;
-            let cubePos = result.cube.cubePos;
-            let cubeType = result.cube.cubeType;
-            let state = games[gameId].state;
-            if (!state)
-                state = {
-                    cubes: []
-                }
-            
-            //state[cubePos] = color;
-            //state.cubes = []
-            state.cubes.push(
-                { pos: cubePos, texture: cubeType }
-            )
-            games[gameId].state = state;
-            console.log(state.cubes)
-
-            
-        }
-
-    })
-
-    //generate a new clientId
-    const clientId = guid();
-    clients[clientId] = {
-        "connection":  connection
-    }
-
+// Create Game
+function createGame (client_Id) {
     const payLoad = {
-        "method": "connect",
+        "method": "create",
         "clientId": clientId
     }
-    //send back the client connect
-    connection.send(JSON.stringify(payLoad))
+
+    ws.send(JSON.stringify(payLoad));
+}
+
+btnCreate.addEventListener("click", e => {
+
+    // const payLoad = {
+    //     "method": "create",
+    //     "clientId": clientId
+    // }
+
+    // ws.send(JSON.stringify(payLoad));
+    createGame()
+})
+
+function joinGame (game_Id) {
+    if (gameId === null)
+        gameId = txtGameId.value;
+    
+    const payLoad = {
+        "method": "join",
+        "clientId": clientId,
+        "gameId": gameId
+    }
+
+    ws.send(JSON.stringify(payLoad));
+}
+
+
+//wiring events
+btnJoin.addEventListener("click", e => {
+
+    // if (gameId === null)
+    //     gameId = txtGameId.value;
+    
+    // const payLoad = {
+    //     "method": "join",
+    //     "clientId": clientId,
+    //     "gameId": gameId
+    // }
+
+    // ws.send(JSON.stringify(payLoad));
+    joinGame()
 
 })
 
 
-function updateGameState(){
 
-    //{"gameid", fasdfsf}
-    for (const g of Object.keys(games)) {
-        const game = games[g]
-        const payLoad = {
-            "method": "update",
-            "game": game
-        }
-
-        game.clients.forEach(c=> {
-            clients[c.clientId].connection.send(JSON.stringify(payLoad))
-        })
+ws.onmessage = message => {
+    //message.data
+    const response = JSON.parse(message.data);
+    //connect
+    if (response.method === "connect"){
+        clientId = response.clientId;
+        console.log("Client id Set successfully " + clientId)
     }
 
-    setTimeout(updateGameState, 5000);
+    //create
+    if (response.method === "create"){
+        gameId = response.game.id;
+        console.log("game successfully created with id " + response.game.id)  
+    }
+
+
+    // Updating state 
+    if (response.method === "update"){
+
+        if (!response.game.state) return;
+       
+
+        for(const b of Object.keys(response.game.state))
+        {
+            state = response.game.state.cubes;
+            for(let i in state) {
+                console.log(i)
+            } 
+            console.log(state)
+        }
+
+    }
+
+    //join
+    if (response.method === "join"){
+        const game = response.game;
+
+        // Displaying html for players nothing fancy
+        while(divPlayers.firstChild)
+            divPlayers.removeChild (divPlayers.firstChild)
+
+        game.clients.forEach (c => {
+
+            const d = document.createElement("div");
+            d.style.width = "200px";
+            d.style.background = c.color
+            d.textContent = c.clientId;
+            divPlayers.appendChild(d);
+
+            if (c.clientId === clientId) playerColor = c.color;
+        })
+
+
+        // Ignore this code
+        while(divBoard.firstChild)
+        divBoard.removeChild (divBoard.firstChild)
+        const b = document.createElement("button");
+        b.textContent = "block"
+        b.style.width = "150px"
+        b.style.height = "150px"
+
+
+        // Call this function when block is placed
+        function onBlockPlaced (cubePos, cubeType) {
+            const payLoad = {
+                "method": "play",
+                "clientId": clientId,
+                "gameId": gameId,
+                "cube" : {
+                    "cubePos": cubePos,
+                    "cubeType": cubeType 
+                } 
+            }
+            ws.send(JSON.stringify(payLoad))
+        }
+
+        b.addEventListener("click", e => {
+            const cubePos = [0, 0, 0];
+            const cubeType = "dirt";
+            onBlockPlaced(cubePos, cubeType)
+            
+        })
+        divBoard.appendChild(b);
+    }
 }
-
-
-
-function S4() {
-    return (((1+Math.random())*0x10000)|0).toString(16).substring(1); 
-}
- 
-// then to call it, plus stitch in '4' in the third group
-const guid = () => (S4() + S4() + "-" + S4() + "-4" + S4().substr(0,3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase();
- 
---
+++ server/server.js
@@ -0,0 +1,127 @@
+const http = require("http");
+
+const websocketServer = require("websocket").server
+const httpServer = http.createServer();
+httpServer.listen(9090, () => console.log("Listening.. on 9090"))
+//hashmap clients
+const clients = {};
+const games = {};
+
+const wsServer = new websocketServer({
+    "httpServer": httpServer
+})
+wsServer.on("request", request => {
+    //connect
+    const connection = request.accept(null, request.origin);
+    connection.on("open", () => console.log("opened!"))
+    connection.on("close", () => console.log("closed!"))
+    connection.on("message", message => {
+        const result = JSON.parse(message.utf8Data)
+        console.log(result)
+        //I have received a message from the client
+        //a user want to create a new game
+        if (result.method === "create") {
+            const clientId = result.clientId;
+            const gameId = guid();
+            games[gameId] = {
+                "id": gameId,
+                "clients": []
+            }
+
+            const payLoad = {
+                "method": "create",
+                "game" : games[gameId]
+            }
+
+            const con = clients[clientId].connection;
+            con.send(JSON.stringify(payLoad));
+        }
+
+        //a client want to join
+        if (result.method === "join") {
+            if(!result.gameId) {
+                return
+            }
+
+            const clientId = result.clientId;
+            const gameId = result.gameId;
+            const game = games[gameId];            
+            game.clients.push({
+                "clientId": clientId,
+            })
+            //start the game
+            if (game.clients.length > 0) updateGameState();
+
+            const payLoad = {
+                "method": "join",
+                "game": game
+            }
+            //loop through all clients and tell them that people has joined
+            game.clients.forEach(c => {
+                clients[c.clientId].connection.send(JSON.stringify(payLoad))
+            })
+        }
+        //a user plays
+        if (result.method === "play") {
+            const gameId = result.gameId;
+            let cubePos = result.cube.cubePos;
+            let cubeType = result.cube.cubeType;
+            let state = games[gameId].state;
+            if (!state)
+                state = {
+                    cubes: []
+                }
+            state.cubes.push(
+                { pos: cubePos, texture: cubeType }
+            )
+            games[gameId].state = state;
+            console.log(state.cubes)
+
+            
+        }
+
+    })
+
+    //generate a new clientId
+    const clientId = guid();
+    clients[clientId] = {
+        "connection":  connection
+    }
+
+    const payLoad = {
+        "method": "connect",
+        "clientId": clientId
+    }
+    //send back the client connect
+    connection.send(JSON.stringify(payLoad))
+
+})
+
+
+function updateGameState(){
+
+    //{"gameid", fasdfsf}
+    for (const g of Object.keys(games)) {
+        const game = games[g]
+        const payLoad = {
+            "method": "update",
+            "game": game
+        }
+
+        game.clients.forEach(c=> {
+            clients[c.clientId].connection.send(JSON.stringify(payLoad))
+        })
+    }
+
+    setTimeout(updateGameState, 5000);
+}
+
+
+
+function S4() {
+    return (((1+Math.random())*0x10000)|0).toString(16).substring(1); 
+}
+ 
+// then to call it, plus stitch in '4' in the third group
+const guid = () => (S4() + S4() + "-" + S4() + "-4" + S4().substr(0,3) + "-" + S4() + "-" + S4() + S4() + S4()).toLowerCase();
+ 
