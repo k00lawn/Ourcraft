4f5ffd89c7012f1851327514f0121dcf1f42df39
k00lawn <k00lawn@gmail.com>
2021-03-09 13:06:00 +0530

    adjusted css for touchcontrol buttons

--- src/components/TouchControls.js
+++ src/components/TouchControls.js
@@ -1,6 +1,8 @@
 import React from "react";
 import { css, jsx } from '@emotion/core';
 import style from '../style'
+import { BiUpArrow, BiRightArrow, BiDownArrow, BiLeftArrow } from 'react-icons/bi'
+import { GiJumpAcross } from "react-icons/gi"
 
 
 
@@ -64,13 +66,44 @@ export const TouchControls = () => {
         opacity: "0.86",
     }
 
+    const gameController = {
+        display: "grid",
+        position: "absolute",
+        bottom: "10px",
+        left: "10px",
+        "grid-template-rows": "125px 125px 125px",
+        "grid-template-columns": "125px 125px 125px",
+    }
+
+    const box = {
+        display: "flex",
+        "align-items": "center",
+        "justify-content": "center",
+        color: "#AAA",
+    }
+
+    const arrows = {
+        "font-size": "75px",
+    }
+
     return (
-        <div style = {{  position: 'absolute', bottom: '20px'  }}>
-        <button style={moveUpBtn}>Up button</button>
-        <button style={moveRightBtn}>Right button</button>
-        <button style={moveDownBtn}>Down Button</button>
-        <button style={moveLeftBtn}>Left Button</button>
-        <button style={jumpBtn}>Jump Button</button>
-      </div> 
+    //     <div style = {{  position: 'absolute', bottom: '20px'  }}>
+    //     <button style={moveUpBtn}>Up button</button>
+    //     <button style={moveRightBtn}>Right button</button>
+    //     <button style={moveDownBtn}>Down Button</button>
+    //     <button style={moveLeftBtn}>Left Button</button>
+    //     <button style={jumpBtn}>Jump Button</button>
+    //   </div> 
+        <div style={gameController}>
+            <div style={box}></div>
+            <div style={box}><button style={moveUpBtn}><BiUpArrow style={arrows} /></button></div>
+            <div style={box}></div>
+            <div style={box}><button style={moveLeftBtn}><BiLeftArrow style={arrows} /></button></div>
+            <div style={box}><button style={jumpBtn}><GiJumpAcross style={{"font-size": "50px"}} /></button></div>
+            <div style={box}><button style={moveRightBtn}><BiRightArrow style={arrows} /></button></div>
+            <div style={box}></div>
+            <div style={box}><button style={moveDownBtn}><BiDownArrow style={arrows} /></button></div>
+            <div style={box}></div>
+        </div>
     )     
 };
