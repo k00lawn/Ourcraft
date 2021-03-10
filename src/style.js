953999aa0062cb5b1f7cf5f09f16b8d7bee6ad14
jaabir <jaabir.zaid@gmail.com>
2021-03-10 15:05:52 +0530

    reduced the btn size

--- src/style.js
+++ src/style.js
@@ -1,136 +1,126 @@
-
-import { css } from '@emotion/core';
+import { css } from "@emotion/core";
 
 export default {
+  menu: css`
+    width: 100%;
+    background-color: white;
+    padding: 15px;
+    border: 2px solid black;
+    display: flex;
+    justify-content: center;
+    font-family: arcadeClassic;
+    font-size: 3rem;
+    opacity: 0.9;
+    &: hover {
+      border: 2px solid white;
+      color: darkgrey;
+    }
+  `,
 
+  hideBtn: css`
+    display: none;
+  `,
+  txtStyle: css`
+    font-family: arcadeClassic;
+    font-size: 30px;
+    text-align: center;
+    @media (max-width: 450px) {
+      font-size: 20px;
+    }
+  `,
+  centerAlign: css`
+    display: flex;
+    justify-content: center;
+  `,
+  inputBoxStyle: css`
+    width: 80%;
+    display: flex;
+    margin: auto;
+    padding: 10px;
+    background-color: white;
+    margin-bottom: 10px;
+  `,
+  btn: css`
+    width: 100%;
+    height: 100%;
+    background-color: grey;
+    //    @media (max-width: 450px ) {
+    //     width: 50px;
+    //     height:50px;
+    //  }
+  `,
+  moveUpBtn: css`
+    top: 673px;
+    left: 157px;
+    border: 3px solid #909090;
+    border-radius: 67px;
+    opacity: 0.86;
+  `,
 
-    menu: css`
-         width: 100%;
-        background-color: white;
-        padding: 15px;
-        border: 2px solid black;
-        display: flex;
-        justify-content: center;
-        font-family: arcadeClassic;
-        font-size: 3rem;
-        opacity: 0.9;
-         &: hover{
-            border: 2px solid white;
-            color: darkgrey;
-        }
-     `,
+  gameController: css`
+    display: grid;
+    position: absolute;
+    bottom: 10px;
+    left: 10px;
+    grid-template-rows: 55px 55px 55px;
+    grid-template-columns: 55px 55px 55px;
 
-     hideBtn: css`
-        display: none;
-     `,
-     txtStyle: css`
-            font-family: arcadeClassic;
-            font-size: 30px;
-            text-align: center;
-        @media (max-width: 450px ) {
-            font-size: 20px;
-        }
-     `,
-     centerAlign: css`
-      display: flex ;
-      justify-content: center;
-     `,
-     inputBoxStyle: css`
-         width: 80%;
-         display: flex;
-         margin: auto;
-         padding: 10px;
-         background-color: white;
-         margin-bottom:10px;
-     `,
-     btn : css`
-        
-            width: 100%;
-            height: 100%;
-          background-color: grey;
-        //    @media (max-width: 450px ) {
-        //     width: 50px;
-        //     height:50px;
-        //  }
-     `,
-     moveUpBtn: css`
-         top: 673px;
-        left: 157px;
-        border: 3px solid #909090;
-        border-radius: 67px;
-        opacity: 0.86
-        `,
+    @media (max-width: 450px) {
+    }
+  `,
 
-     gameController : css`
-       display: grid;
-        position: absolute;
-        bottom: 10px;
-        left: 10px;
-        grid-template-rows: 70px 70px 70px;
-        grid-template-columns: 70px 70px 70px;
+  moveRightBtn: css`
+    left: 280px;
+    border: 3px solid #909090;
+    border-radius: 67px;
+    opacity: 0.86;
+  `,
 
-         @media (max-width: 450px ) {
+  moveDownBtn: css`
+    top: 921px;
+    left: 156px;
+    border: 3px solid #909090;
+    border-radius: 67px;
+    opacity: 0.86;
+  `,
 
-         }
-     `,
-    
-    moveRightBtn: css`
-         left: 280px;
-         border: 3px solid #909090;
-        border-radius: 67px;
-        opacity: 0.86;
-        
-    `,
+  moveLeftBtn: css`
+    top: 921px;
+    left: 156px;
+    border: 3px solid #909090;
+    border-radius: 67px;
+    opacity: 0.86;
+  `,
 
-    moveDownBtn: css`
-         top: 921px;
-        left: 156px;
-        border: 3px solid #909090;
-        border-radius: 67px;
-        opacity: 0.86;
-        `,
+  jumpBtn: css`
+    top: 921px;
+    left: 156px;
 
-    moveLeftBtn: css`
-        top: 921px;
-        left: 156px;
-        border: 3px solid #909090;
-        border-radius: 67px;
-        opacity: 0.86;
-        `,
+    border: 3px solid #909090;
+    border-radius: 67px;
+    opacity: 0.86;
+  `,
 
-    jumpBtn: css`
-       top: 921px;
-        left: 156px;
-      
-        border: 3px solid #909090;
-        border-radius: 67px;
-        opacity: 0.86;
-        `,
-
-     box : css`
-      display: flex;
-        align-items: center;
-        justify-content: center;
-        color: #AAA;
-          width: 124px;
-          height: 125px;
-        @media (max-width: 450px ) {
-            width: 50px;
-            height:50px;
-         }
-
-    `,
-    arrows : css`
-     font-size: 2rem;
-    `,
-    ctaBtn: css`
-        @media (max-width: 450px ) {
-                height: 50px;
-                font-size: 1.3rem;
-                align-items: center;
-        }
-       
-    
-    `
- 
-}
+  box: css`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #aaa;
+    width: 50px;
+    height: 50px;
+    @media (max-width: 450px) {
+      width: 50px;
+      height: 50px;
+    }
+  `,
+  arrows: css`
+    font-size: 2rem;
+  `,
+  ctaBtn: css`
+    @media (max-width: 450px) {
+      height: 50px;
+      font-size: 1.3rem;
+      align-items: center;
+    }
+  `,
+};
